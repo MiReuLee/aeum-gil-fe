6eba5dc3b78b3cfeb65b758b42d6b405d5e3d8ac
Mi Reu Lee <im490113@gmail.com>
2025-04-23 21:15:58 +0900

    엔딩 데이터 연동 임시 처리 (우선 내용만 나오도록)

--- src/pages/Ending/index.tsx
+++ src/pages/Ending/index.tsx
@@ -18,7 +18,16 @@ export const Ending = () => {
 
   useEffect(() => {
     if (endings.length) {
-      setEnding(endings[Number(endingId) - 1] as $Ending);
+      const _ending = endings[Number(endingId) - 1] as $Ending;
+
+      if (_ending) {
+        const { text, img } = JSON.parse(_ending.content);
+        setEnding({
+          ..._ending,
+          text,
+          img,
+        });
+      }
     }
   }, [endings, endingId]);
 
@@ -34,6 +43,11 @@ export const Ending = () => {
     <Grid2 container>
       <MainBg gap={'0'} colorBg='dark'>
         <h1 className="old-newspaper" style={{ color: '#ebebeb', fontSize: '88px' }}>The End</h1>
+        
+        <Grid2 container color={'#ebebeb'}>
+          <Grid2>{ending?.title}</Grid2>
+          <Grid2>{ending?.text}</Grid2>
+        </Grid2>
 
         <ButtonUsage onClick={handleClick}>
           <span>
